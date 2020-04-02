af653223dbb806d4bf2a2d1e918e7e50f1846ba3
Suffren <regis.cleis@hotmail.fr>
2020-04-02 16:15:54 +0200

    create a "manage-reports" component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
+import { ManageReportsComponent } from './manage-reports/manage-reports.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    ManageReportsComponent
   ],
   imports: [
     CommonModule,
