0315ad8653ef9f0c4d49c031fb12323bbe6d07de
Suffren <regis.cleis@hotmail.fr>
2020-04-02 16:16:48 +0200

    create a "manage-users" component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { ManageReportsComponent } from './manage-reports/manage-reports.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    ManageReportsComponent
+    ManageReportsComponent,
+    ManageUsersComponent
   ],
   imports: [
     CommonModule,
