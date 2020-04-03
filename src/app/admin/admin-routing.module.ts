6458596e703259d9f113b4eddcae2f7c6548628b
Suffren <regis.cleis@hotmail.fr>
2020-04-03 10:23:41 +0200

    set an auth.guard and set it on admin-routing.module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { ManageReportsComponent } from './manage-reports/manage-reports.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const adminRoutes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
