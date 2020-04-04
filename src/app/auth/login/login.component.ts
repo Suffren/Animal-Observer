ce75632acd20b40c7e1fb813204522d475a76587
Suffren <regis.cleis@hotmail.fr>
2020-04-04 12:37:45 +0200

    login.component: set a formBuilder with email validator

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,20 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
   message: string;
+  loginForm: FormGroup;
+  submitted: boolean = false;
 
-  constructor(public authService: AuthService, public router: Router) {
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    private fb: FormBuilder,
+  ) {
     this.setMessage();
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]],
+      password: ['', Validators.required]
+    });
   }
 
   setMessage() {
@@ -35,4 +47,6 @@ export class LoginComponent {
     this.authService.logout();
     this.setMessage();
   }
+
+  onSubmit() { }
 }
