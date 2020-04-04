14f81b49fa5dbc2680d3bba9bf34f0e56c37c6a2
Suffren <regis.cleis@hotmail.fr>
2020-04-04 12:33:46 +0200

    auth.module: use reactive forms instead of basic forms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { CommonModule }   from '@angular/common';
-import { FormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent }    from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
@@ -8,7 +8,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ],
   declarations: [
