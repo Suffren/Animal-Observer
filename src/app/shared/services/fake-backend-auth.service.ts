7977f5c3fa070f5cd4909c3888a5e7fdc5f4d7ec
Suffren <regis.cleis@hotmail.fr>
2020-04-08 11:55:27 +0200

    fake-backend-auth: when trying to logged in, check if user's credentials are correct doing an http request on 'api/users' rather than checking 'users.json'

--- src/app/shared/services/fake-backend-auth.service.ts
+++ src/app/shared/services/fake-backend-auth.service.ts
@@ -1,39 +1,45 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
-import  Users  from  '../users.json';
-const users = Users;
+import  User  from  '../interfaces/interfaces';
+import { UserService } from './user.service';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class fakeBackendAuthService implements HttpInterceptor {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    if (request.url.endsWith('authentification') && request.method === 'POST') {
-      
-      let filteredUsers = users.filter(user => {
-        return user.email === request.body.email && user.password === request.body.password;
-      });
+    if (request.url ==='authentification' && request.method === 'POST') {
 
-      if (filteredUsers.length) {
-        // if login details are valid return status 200 with fake jwt token
-        let user = filteredUsers[0];
-        let body = {
-            id: user.id,
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            token: 'fake-jwt-token'
-        };
+      return this.userService.getAll().pipe(
+        switchMap( users => {
+          let filteredUsers = users.filter(user => {
+            return user.email === request.body.email && user.password === request.body.password;
+          });
 
-        return of(new HttpResponse({ status: 200, body: body }));
-      } else {
-          return throwError({ message: 'Email ou mot de passe incorrect' });
-      }
+          if (filteredUsers.length) {
+            // if login details are valid return status 200 with fake jwt token
+            let user = filteredUsers[0];
+            let body = {
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: 'fake-jwt-token'
+            };
+
+            return of(new HttpResponse({ status: 200, body: body }));
+          } else {
+            return throwError({ message: 'Email ou mot de passe incorrect' });
+          }
+        })
+      );
     }
 
     return next.handle(request);
