a20913c784e56f117b785f3b30a1ea867e553a53
Suffren <regis.cleis@hotmail.fr>
2020-04-11 15:32:06 +0200

    manage-users: display users and set some layout

--- src/app/admin/manage-users/manage-users.component.ts
+++ src/app/admin/manage-users/manage-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/services/user.service';
+import User from '../../shared/interfaces/interfaces'
 
 @Component({
   selector: 'app-manage-users',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-users.component.scss']
 })
 export class ManageUsersComponent implements OnInit {
+  users:  User[];
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
+  getUsers() {
+    this.isLoading = true;
+    this.userService.getAll().subscribe( users => {
+      this.isLoading = false;
+      this.users = users;
+    })
+  }
 }
