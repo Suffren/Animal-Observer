ca8ea7bb5c6e05318c2111744f9c7d08ffc16cd0
Suffren <regis.cleis@hotmail.fr>
2020-04-26 10:06:17 +0200

    reports components: prepare the component to be reuse for admin users (can delete and edit reports)

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import Report from '../shared/interfaces/interfaces';
 import User from '../shared/interfaces/interfaces';
+
 import { ReportService } from '../shared/services/report.service';
 import { AuthService } from '../auth/auth.service';
+
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+
 import { tap } from "rxjs/operators";
 
 registerLocaleData(localeFr, 'en');
@@ -18,21 +23,41 @@ export class ReportsComponent implements OnInit {
   reports: Array<Report> = [];
   isLoading: boolean;
   currentUser: User;
+  currentUrl: string;
 
   constructor(
     private reportService: ReportService,
-    private authService: AuthService
+    private authService: AuthService,
+    private route: Router
   ) { }
 
   ngOnInit(): void {
-    this.authService.currentUserValue.subscribe( user => this.currentUser = user);
     this.isLoading = true;
-    this.reportService.getReportsByUser(this.currentUser.id).pipe(
-      tap(reports => this.isLoading = false)
-    ).subscribe( (reports) => {
-      this.reports = reports.sort( (previousReport: Report, nextReport: Report) =>
-        new Date(nextReport.time).getTime() - new Date(previousReport.time).getTime()
-      );
+    this.currentUrl = this.route.url;
+
+    this.authService.currentUserValue.subscribe( user => {
+      this.currentUser = user;
+
+      if(this.currentUser.isAdmin === 'true' && this.currentUrl === '/admin/reports') {
+        this.reportService.fetchReports().pipe(
+          tap(reports => this.isLoading = false)
+        ).subscribe( (reports) => {
+          this.reports = this.reportsByDate(reports);
+        });
+      } else {
+        this.reportService.getReportsByUser(this.currentUser.id).pipe(
+          tap(reports => this.isLoading = false)
+        ).subscribe( (reports) => {
+          this.reports = this.reportsByDate(reports);
+        });
+      }
+
     });
+  }
+
+  reportsByDate(reports: Report[]): Report[] {
+    return reports.sort( (previousReport: Report, nextReport: Report) =>
+      new Date(nextReport.time).getTime() - new Date(previousReport.time).getTime()
+    );
   }
 }
