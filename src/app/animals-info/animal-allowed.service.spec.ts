abc2ddad464c3c8a2accbb2a0f4074cd68e06560
Suffren <regis.cleis@hotmail.fr>
2020-05-04 11:29:16 +0200

    animals-info: make the tests passed

--- src/app/animals-info/animal-allowed.service.spec.ts
+++ src/app/animals-info/animal-allowed.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AnimalAllowedService } from './animal-allowed.service';
 
@@ -6,7 +7,7 @@ describe('AnimalAllowedService', () => {
   let service: AnimalAllowedService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [RouterTestingModule] });
     service = TestBed.inject(AnimalAllowedService);
   });
 
--- src/app/animals-info/animals-info-list/animals-info-list.component.spec.ts
+++ src/app/animals-info/animals-info-list/animals-info-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AnimalsInfoListComponent } from './animals-info-list.component';
 
@@ -8,7 +10,8 @@ describe('AnimalsInfoListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnimalsInfoListComponent ]
+      declarations: [AnimalsInfoListComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   }));
