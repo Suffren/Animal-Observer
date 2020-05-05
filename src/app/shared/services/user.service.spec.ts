69406b67b1e7d8d67d286fb463afbf1fe78535bd
Suffren <regis.cleis@hotmail.fr>
2020-05-05 12:09:30 +0200

    user.service.spec: testing 'getAll' method

--- src/app/shared/services/user.service.spec.ts
+++ src/app/shared/services/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { UserService } from './user.service';
 
@@ -17,4 +18,39 @@ describe('UserService', () => {
     expect(service.usersUrl).toEqual('api/users');
   });
 
+  describe('all', () => {
+    it('should return a collection of users', () => {
+      const usersResponse = [
+        {
+          "id": 1,
+          "email": "j@j.jr",
+          "username": "JD",
+          "password": "qwerty",
+          "firstname": "John ",
+          "lastname": "Doe",
+          "isAdmin": false,
+          "token": "fake-token"
+        },
+        {
+          "id": 2,
+          "email": "t@t.tr",
+          "username": "JD",
+          "password": "azerty",
+          "firstname": "Jean",
+          "lastname": "Dupond",
+          "isAdmin": true,
+          "token": "fake-token"
+        }
+      ];
+
+      let response;
+      spyOn(service, 'getAll').and.returnValue(of(usersResponse));
+
+      service.getAll().subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(usersResponse);
+    });
+  });
 });
